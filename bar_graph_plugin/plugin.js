07775ec42f254d6fd118c827271acde2c3344fb1
Bryan Lunt <bjlunt2@illinois.edu>
2017-06-17 14:02:34 -0500

    displays nothing

--- bar_graph_plugin/plugin.js
+++ bar_graph_plugin/plugin.js
@@ -1,7 +1,7 @@
-    function BarGraphController($scope, telemetryHandler) {
+    function BarGraphController($scope, dialogService, telemetryHandler) {
         var handle;
-
         // Add min/max defaults
+
         $scope.low = -1;
         $scope.middle = 0;
         $scope.high = 1;
@@ -12,16 +12,34 @@
             return Math.min(100, Math.max(0, pct));
         };
 
+        console.dir(dialogService);
+        console.dir(telemetryHandler);
+
+        /*
         // Use the telemetryHandler to get telemetry objects here
         handle = telemetryHandler.handle($scope.domainObject, function () {
+            console.log("handle function called.");
             $scope.telemetryObjects = handle.getTelemetryObjects();
             $scope.barWidth =
                 100 / Math.max(($scope.telemetryObjects).length, 1);
         });
 
+        // Get bottom and top (as percentages) for current value
+        $scope.getBottom = function (telemetryObject) {
+            var value = handle.getRangeValue(telemetryObject);
+            return $scope.toPercent(Math.min($scope.middle, value));
+        };
+
+        $scope.getTop = function (telemetryObject) {
+            var value = handle.getRangeValue(telemetryObject);
+            return 100 - $scope.toPercent(Math.max($scope.middle, value));
+        };
+
         // Release subscriptions when scope is destroyed
         $scope.$on('$destroy', handle.unsubscribe);
-    }
+        */
+
+}
 
 
 
@@ -39,6 +57,17 @@ function BarGraphPlugin(){
                 creatable: true,
                 initialize: function(object){
                   //object.limits = {"min":-1000, "max":1000}
+                },
+                "delegates" : [
+                  "telemetry"
+                ],
+                "contains": [
+                        {
+                            "has": "telemetry"
+                        }
+                    ],
+                "model":{
+                  "composition":[]
                 }
             });
 
@@ -58,11 +87,23 @@ function BarGraphPlugin(){
     );
   openmct.legacyExtension("stylesheets",
           {
+              //TODO: All of this may not be necessary, since the stylesheets use CSS classes...
+              //I wonder if the system will use them to do anything at all to limit application of
+              //this css?
+              name: "Bar Graph stylesheet",
+              key: "example.bargraph",
+              type: "example.bargraph",
               //TODO: Complain to upstream developers about bad stylesheetUrl resolution.
               "stylesheetUrl": "../../../../../bar_graph_plugin/res/css/bargraph.css"
           }
         );
 
+        openmct.legacyExtension("controllers",
+        {
+                 key: "BarGraphController",
+                 "implementation": BarGraphController,
+                 "depends": [ "$scope", "dialogService", "telemetryHandler" ]
+             });
 
   };
 }
