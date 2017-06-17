c40a409d0b86dd535a61ee212f5abecd20d43c53
Bryan Lunt <bjlunt2@illinois.edu>
2017-06-17 11:12:57 -0500

    view no longer overrides other views

--- bar_graph_plugin/plugin.js
+++ bar_graph_plugin/plugin.js
@@ -1,3 +1,29 @@
+    function BarGraphController($scope, telemetryHandler) {
+        var handle;
+
+        // Add min/max defaults
+        $scope.low = -1;
+        $scope.middle = 0;
+        $scope.high = 1;
+
+        // Convert value to a percent between 0-100, keeping values in points
+        $scope.toPercent = function (value) {
+            var pct = 100 * (value - $scope.low) / ($scope.high - $scope.low);
+            return Math.min(100, Math.max(0, pct));
+        };
+
+        // Use the telemetryHandler to get telemetry objects here
+        handle = telemetryHandler.handle($scope.domainObject, function () {
+            $scope.telemetryObjects = handle.getTelemetryObjects();
+            $scope.barWidth =
+                100 / Math.max(($scope.telemetryObjects).length, 1);
+        });
+
+        // Release subscriptions when scope is destroyed
+        $scope.$on('$destroy', handle.unsubscribe);
+    }
+
+
 
 function BarGraphPlugin(){
   return function install(openmct){
@@ -20,6 +46,8 @@ function BarGraphPlugin(){
             {
                 "name": "Bar Graph",
                 "key": "example.bargraph",
+                //TODO: If a type is not specified, the view is applied to every type.
+                "type": "example.bargraph",
                 //TODO: Complain to upstream developers about bad templateUrl resolution.
                 "templateUrl": "../../bar_graph_plugin/res/templates/bargraph.html",
                 //Error: no services to agregate ?
